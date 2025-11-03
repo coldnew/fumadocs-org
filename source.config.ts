e8c176c004946c009e19a1370faa33ed1d153f9a
Yen-Chin, Lee <coldnew.tw@gmail.com>
2025-11-03 11:43:41 +0800

    refactor: move org remark plugin to org-mode library

--- source.config.ts
+++ source.config.ts
@@ -1,5 +1,5 @@
 import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
-import { orgRemarkPlugin } from '@/lib/org-remark-plugin';
+import { orgRemarkPlugin } from '@/lib/org-mode';
 
 // Options: https://fumadocs.vercel.app/docs/mdx/collections#define-docs
 export const docs = defineDocs({
--- src/lib/org-mode/index.ts
+++ src/lib/org-mode/index.ts
@@ -1,3 +1,4 @@
 export { convertOrgToMdx } from './converter';
+export { orgRemarkPlugin } from './org-remark-plugin';
 export type { OrgKeywords, ConversionOptions, ConversionResult } from './types';
 export { extractOrgKeywords, generateDefaultTitle } from './utils';
